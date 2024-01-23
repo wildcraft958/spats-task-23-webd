71cd6f4d07bf45afd6173df594d3e4df8fb8098e
Animesh <animeshraj958@gmail.com>
2024-01-24 00:17:35 +0530

    c3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,17 +16,6 @@ function Navbar() {
           <li class="nav-item">
             <a class="nav-link" href="/">spAts</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="/">Action</a></li>
-              <li><a class="dropdown-item" href="/">Another action</a></li>
-              <li><hr class="dropdown-divider"/></li>
-              <li><a class="dropdown-item" href="/">Something else here</a></li>
-            </ul>
-          </li>
         </ul>
         <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
